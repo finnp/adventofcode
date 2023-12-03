2c15232ddf678cd0b1469c3031b6fba09cae8473
Finn Pauls <mbp@pauls.dev>
2023-12-04 00:28:41 +0100

    2022 - Day 6 - Part 2

--- 2022/day6.hs
+++ 2022/day6.hs
@@ -2,20 +2,19 @@
 
 main = do
     contents <- getContents
-    -- let example = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
 
-    print $ find_first_four_different contents 4
+    print $ find_first_14_different contents 14
 
 --
 
-find_first_four_different :: String -> Int -> Int
-find_first_four_different (x:xs) position = 
-    if are_first_four_different (x:xs) 
+find_first_14_different :: String -> Int -> Int
+find_first_14_different (x:xs) position = 
+    if are_first_n_different 14 (x:xs) 
         then position 
-        else find_first_four_different xs (position + 1)
+        else find_first_14_different xs (position + 1)
 
-are_first_four_different :: String -> Bool
-are_first_four_different (a:(b:(c:(d:rest)))) = are_all_different [a,b,c,d]
+are_first_n_different :: Int -> String -> Bool
+are_first_n_different n list  = are_all_different (take n list)
 
 are_all_different :: String -> Bool
 are_all_different (x:xs) =  (is_different_from_rest x xs) && are_all_different xs
