1ff2d99db17eff16df0af472c82252215bf7d9e3
Finn Pauls <mbp@pauls.dev>
2023-12-03 22:03:18 +0100

    2022 - Day 5 - Part 2

--- 2022/day5.hs
+++ 2022/day5.hs
@@ -60,18 +60,17 @@ applyMovements [] stack = stack
 
 move :: Movement -> [Stack] -> [Stack]
 move (numberOfCrates, fromPosition, toPosition) stack = do
-    let stackAfterOneMove = moveOne fromPosition toPosition stack
-    if numberOfCrates == 1 then
-        stackAfterOneMove
-    else
-        move ((numberOfCrates - 1), fromPosition, toPosition) stackAfterOneMove
+    let load = getTopN numberOfCrates fromPosition stack
+    let stackWithout = removeNFromPosition numberOfCrates fromPosition stack
+    addToPosition toPosition load stackWithout
 
-moveOne :: Int -> Int -> [Stack] -> [Stack]
-moveOne fromPosition toPosition stack = do
-    let topChar = getTop fromPosition stack
-    let withRemoved = removeFromPosition fromPosition stack
-    addToPosition toPosition topChar withRemoved
 
+getTopN :: Int -> Int -> [Stack] -> String
+getTopN number position stack = take number (snd (stack !! (position - 1)))
+
+removeNFromPosition :: Int -> Int -> [Stack] -> [Stack]
+removeNFromPosition 1 position stack = removeFromPosition position stack
+removeNFromPosition number position stack = removeNFromPosition (number - 1) position (removeFromPosition position stack)
 
 getTop :: Int -> [Stack] -> Char
 getTop position stack = head (snd (stack !! (position - 1)))
@@ -79,15 +78,15 @@ getTop position stack = head (snd (stack !! (position - 1)))
 removeFromPosition :: Int -> [Stack] -> [Stack]
 removeFromPosition position stack = map (removeIfPositionMatches position) stack
 
-addToPosition  :: Int -> Char -> [Stack] -> [Stack]
-addToPosition position character stack = map (addIfPositionMatches position character) stack
+addToPosition  :: Int -> String -> [Stack] -> [Stack]
+addToPosition position characters stack = map (addIfPositionMatches position characters) stack
 
 removeIfPositionMatches :: Int -> Stack -> Stack
 removeIfPositionMatches matchPosition (position, (a:stack))  = if position == matchPosition then (position,stack) else (position,a:stack)
 removeIfPositionMatches matchPosition (position, []) = (position, [])
 
-addIfPositionMatches :: Int -> Char -> Stack -> Stack
-addIfPositionMatches matchPosition char (position, stack)  = if position == matchPosition then (position,char:stack) else (position,stack)
+addIfPositionMatches :: Int -> String -> Stack -> Stack
+addIfPositionMatches matchPosition chars (position, stack)  = if position == matchPosition then (position,chars ++ stack) else (position,stack)
 
 removeTop :: String -> String
 removeTop (a:as) = as
