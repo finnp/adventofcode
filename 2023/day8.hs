698e3e33ed9ac4c2347edd93aae42997e335b95e
Finn Pauls <mbp@pauls.dev>
2023-12-08 11:01:37 +0100

    Inline scanl

--- 2023/day8.hs
+++ 2023/day8.hs
@@ -11,15 +11,12 @@ main = do
     let instructions = rows !! 0
     let network = Map.fromList $ map (prepareForMap . words . cleanString) $ drop 2 rows
 
-    print $ length $ takeWhile (/="ZZZ") $ lazyFollow network "AAA" (cycle instructions)
+    print $ length $ takeWhile (/="ZZZ") $ scanl (follow network) "AAA" (cycle instructions)
     
 
 follow :: Map.Map String (String, String) -> String -> Char -> String
 follow network id 'R' = snd $ fromJust $ Map.lookup id network
 follow network id 'L' = fst $ fromJust $ Map.lookup id network
-
-lazyFollow :: Map.Map String (String, String) -> String -> [Char] -> [String]
-lazyFollow network id commands = scanl (follow network) id commands
 
 cleanString :: String -> String
 cleanString [] = []
