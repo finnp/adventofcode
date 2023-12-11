0dbacb04b59b875e340e852f9a3fe25a9584c94d
Finn Pauls <mbp@pauls.dev>
2023-12-11 23:30:00 +0100

    2023 - Day 11 - Part 1

--- 2023/day11/day11.hs
+++ 2023/day11/day11.hs
@@ -1,13 +1,16 @@
 import Data.List
 
-type Universe = [[Char]]
+type Universe = [Row]
+type Position = (Int, Int)
+type Row = [Char]
 
 main = do
     content <- getContents
     let universe = lines content
-    printUniverse universe
     putStrLn "\n"
-    printUniverse $ expand universe
+    let expandedUniverse = expand universe
+    printUniverse expandedUniverse
+    print $ sum $ map distance $ combinations $ getGalaxyPositions 0 expandedUniverse
 
 
 flipUniverse :: Universe -> Universe
@@ -17,8 +20,27 @@ expand :: Universe -> Universe
 expand universe = expandEmptyRows $ flipUniverse $ expandEmptyRows . flipUniverse $ universe 
 
 expandEmptyRows :: Universe -> Universe
-expandEmptyRows (row:rows) = if all (=='.') row then (row:(row:(expandEmptyRows rows))) else (row:(expandEmptyRows rows))
 expandEmptyRows [] = []
+expandEmptyRows (row:rows)
+  | all (== '.') row = row : row : expandEmptyRows rows
+  | otherwise        = row : expandEmptyRows rows
+
+getGalaxyPositions :: Int -> Universe -> [Position]
+getGalaxyPositions _ [] = []
+getGalaxyPositions y (row: rows) = map (\x -> (x,y)) (getGalaxyIndeces 0 row) ++ (getGalaxyPositions (y+1)rows)
+
+getGalaxyIndeces :: Int -> Row -> [Int]
+getGalaxyIndeces _ [] = []
+getGalaxyIndeces startIndex (x:xs) 
+    | x == '#'     = startIndex : (getGalaxyIndeces (startIndex + 1) xs)
+    | otherwise    = getGalaxyIndeces (startIndex + 1) xs
+
+distance :: (Position, Position) -> Int
+distance ((ax,ay), (bx,by)) = (abs (ax-bx)) + (abs (ay - by))
 
 printUniverse u = putStrLn $ intercalate "\n" u
     
+combinations :: [Position] -> [(Position, Position)]
+combinations [] = []
+combinations (a:as) = [(a, x)|  x <- as] ++ combinations as
+
